ad5449b9032e6b7e74f6e4da598d5125dd45d242
vietddviajsc <viet307work@gmail.com>
2026-03-10 17:50:05 +0700

    update permission

---
+++ TellMe.Application/Features/Permission/Queries/GetAllRolePermissionQuery.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TellMe.Application.DTOs;
+using TellMe.Core.Common;
+
+namespace TellMe.Application.Features.Permission.Queries
+{
+    public class GetALlRolePermissionQuery : IRequest<Result<List<PermissionDto>>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public GetALlRolePermissionQuery() { } 
+
+        public GetALlRolePermissionQuery(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
